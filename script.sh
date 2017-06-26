e8bff0edba7a2854ade3e6cf47c1627f600737e7
Veeresh Elango <VeereshElango@users.noreply.github.com>
2017-06-26 13:17:20 +0200

    update

--- script.sh
+++ script.sh
@@ -1,1 +1,9 @@
-python3 mnist_mlp.py
+#!/bin/bash
+
+source /etc/profile.d/modules.sh
+
+module load gcc/5.3.0
+
+module load cuda/8.0
+
+THEANO_FLAGS='floatX=float32,device=gpu,nvcc.flags=D_FORCE_INLINES,dnn.enabled=True' python3 mnist_mlp.py
