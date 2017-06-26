1540fbf555733834b6ee0bbd2b2dbb6122925523
Veeresh Elango <VeereshElango@users.noreply.github.com>
2017-06-26 15:34:52 +0200

    added -

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ module load gcc/5.3.0
 
 module load cuda/8.0
 
-THEANO_FLAGS='floatX=float32,device=gpu,nvcc.flags=D_FORCE_INLINES,dnn.enabled=True' python3 mnist_mlp.py
+THEANO_FLAGS='floatX=float32,device=gpu,nvcc.flags-=D_FORCE_INLINES,dnn.enabled=True' python3 mnist_mlp.py
